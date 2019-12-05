085e89639a3c38b0ace4d474a66798c4b00926a4
shaiksha094 <56708657+shaiksha094@users.noreply.github.com>
2019-12-05 08:29:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("well come to banglore");
-        System.out.println("well come to");
-      System.out.println("shaik");
+        System.out.println("hello");
+      System.out.println("lalalal");
 	}
 }
